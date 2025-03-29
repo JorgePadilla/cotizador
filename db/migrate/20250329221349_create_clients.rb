c9670e4d545c8ee7508bca13ae33c391b1e5af31
JorgePadilla <jorgep4dill4@gmail.com>
2025-03-29 19:20:08 -0400

    Add database models and schema for clients, products, suppliers, invoices and invoice items

---
+++ db/migrate/20250329221349_create_clients.rb
@@ -0,0 +1,15 @@
+class CreateClients < ActiveRecord::Migration[8.0]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    
+    create_table :clients, id: :uuid do |t|
+      t.string :name
+      t.string :rtn
+      t.text :address
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20250329221440_create_suppliers.rb
@@ -0,0 +1,15 @@
+class CreateSuppliers < ActiveRecord::Migration[8.0]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    
+    create_table :suppliers, id: :uuid do |t|
+      t.string :name
+      t.string :rtn
+      t.string :contact_name
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
