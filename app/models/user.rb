ad3930350ee7ee20b707166f52b9b19336fd7943
JorgePadilla <jorgep4dill4@gmail.com>
2025-03-30 21:22:08 -0400

    Fix style issues in profiles controller and user model
    - Fixed trailing whitespace
    - Fixed string literal style to use double quotes
    - All tests remain passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
-  
+
   # Validations
   validates :language, inclusion: { in: %w[en es] }
-  
+
   # Set default language
   after_initialize :set_default_language, if: :new_record?
-  
+
   private
-  
+
   def set_default_language
-    self.language ||= 'en'
+    self.language ||= "en"
   end
 end
