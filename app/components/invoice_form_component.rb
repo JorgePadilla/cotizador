1c90078db3008b7a19c2e0ba2ad0eadb1717ad1e
JorgePadilla <jorgep4dill4@gmail.com>
2025-06-22 14:33:54 -0400

    Add nested attributes to Invoice model and add supplier address migration

--- app/components/invoice_form_component.rb
+++ app/components/invoice_form_component.rb
@@ -1,10 +1,22 @@
 class InvoiceFormComponent < ViewComponent::Base
-  def initialize(invoice:, clients:)
+  def initialize(invoice:, clients:, current_user: nil)
     @invoice = invoice
     @clients = clients
+    @current_user = current_user
+  end
+
+  def default_tax_amount
+    return 0 unless @current_user&.default_tax
+    
+    # If invoice is new and has no tax set, use the user's default
+    if @invoice.new_record? && @invoice.tax.nil?
+      (@invoice.subtotal || 0) * @current_user.default_tax
+    else
+      @invoice.tax || 0
+    end
   end
 
   private
 
-  attr_reader :invoice, :clients
+  attr_reader :invoice, :clients, :current_user
 end
