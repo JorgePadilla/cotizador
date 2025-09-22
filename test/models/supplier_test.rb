605d8ada38c3f54a7171bd52734039643d1ad6d7
JorgePadilla <jorgep4dill4@gmail.com>
2025-09-22 01:15:27 -0400

    Add suppliers tests

--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -2,43 +2,51 @@ require "test_helper"
 
 class SupplierTest < ActiveSupport::TestCase
   test "valid supplier" do
+    organization = organizations(:default_organization)
     supplier = Supplier.new(
       name: "Test Supplier",
       rtn: "87654321",
       contact_name: "Contact Person",
       phone: "555-5678",
-      email: "supplier@example.com"
+      email: "supplier@example.com",
+      organization: organization
     )
     assert supplier.valid?
   end
 
   test "supplier without name is invalid" do
+    organization = organizations(:default_organization)
     supplier = Supplier.new(
       rtn: "87654321",
       contact_name: "Contact Person",
       phone: "555-5678",
-      email: "supplier@example.com"
+      email: "supplier@example.com",
+      organization: organization
     )
     assert_not supplier.valid?
   end
 
   test "supplier without rtn is invalid" do
+    organization = organizations(:default_organization)
     supplier = Supplier.new(
       name: "Test Supplier",
       contact_name: "Contact Person",
       phone: "555-5678",
-      email: "supplier@example.com"
+      email: "supplier@example.com",
+      organization: organization
     )
     assert_not supplier.valid?
   end
 
   test "supplier with invalid email is invalid" do
+    organization = organizations(:default_organization)
     supplier = Supplier.new(
       name: "Test Supplier",
       rtn: "87654321",
       contact_name: "Contact Person",
       phone: "555-5678",
-      email: "invalid-email"
+      email: "invalid-email",
+      organization: organization
     )
     assert_not supplier.valid?
   end
